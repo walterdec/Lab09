89ccd935349ba65328aa009abda6d86504ed7221
Alberto Monge <alberto.monge@polito.it>
2019-05-14 09:28:01 +0200

    Progetto Base

---
+++ src/it/polito/tdp/borders/BordersController.java
@@ -0,0 +1,43 @@
+/**
+ * Skeleton for 'Borders.fxml' Controller Class
+ */
+
+package it.polito.tdp.borders;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.borders.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class BordersController {
+
+	Model model;
+
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
+
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
+
+	@FXML // fx:id="txtAnno"
+	private TextField txtAnno; // Value injected by FXMLLoader
+
+	@FXML // fx:id="txtResult"
+	private TextArea txtResult; // Value injected by FXMLLoader
+
+	@FXML
+	void doCalcolaConfini(ActionEvent event) {
+
+		txtResult.setText("Todo!");
+	}
+
+	@FXML // This method is called by the FXMLLoader when initialization is complete
+	void initialize() {
+		assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'Borders.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Borders.fxml'.";
+	}
+}
---
+++ src/it/polito/tdp/borders/Main.java
@@ -0,0 +1,31 @@
+package it.polito.tdp.borders;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Borders.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+			BordersController controller = loader.getController();
+
+			// TODO set model
+
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
